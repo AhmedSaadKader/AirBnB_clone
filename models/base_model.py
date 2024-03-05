043bd9d60bfb4427309f46540cb7c37fad03b40f
moharafat <144336471+moharafat@users.noreply.github.com>
2024-03-05 20:42:02 +0200

    esta3ed llesaytara

---
+++ models/base_model.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""DEfining gthe class BaseModel"""
+import json
+import uuid
+from datetime import datetime
+
+class BaseModel:
+    """Representing the base model"""
+
+    def __init__(self):
+        """Initializing a new Rectangle"""
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now
+        self.updated_at = datetime.now
+    def __str__(self):
+        """Returns the string rep. of the base class"""
+        return f"[{type(self).__name__}] ({self.id}) {self.__dict__}"
+    def save(self):
+        """updates the instance attribute updated_at with cuurent time"""
+        self.updated_at = datetime.now()
+    def to_dict(self):
+        """Returns the dictionary representation of a Baseclass"""
+        return {
+            'id': self.id,
+            'created_at': self.created_at,
+            'updated_at': self.updated_at
+        }
+if __name__ == "__main__":
+    base_model = BaseModel()
+    print(base_model)
