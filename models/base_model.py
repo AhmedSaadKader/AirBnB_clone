928410bd407f3adc94d4bcfa60cfd2826f7b893b
moharafat <144336471+moharafat@users.noreply.github.com>
2024-03-09 23:29:38 +0200

    task 4

--- models/base_model.py
+++ models/base_model.py
@@ -7,11 +7,23 @@ from datetime import datetime
 class BaseModel:
     """Representing the base model"""
 
-    def __init__(self):
+    def __init__(self, **kwargs):
         """Initializing a new Rectangle"""
         self.id = str(uuid.uuid4())
-        self.created_at = datetime.now
-        self.updated_at = datetime.now
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+        if len(kwargs) != 0:
+            for key, value in kwargs.items():
+                if key != "__class__":
+                    if key in ["created_at", "updated_at"]:
+                        setattr(self, key, datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f"))
+                    else:
+                        setattr(self, key, value)
+        if not kwargs:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+
     def __str__(self):
         """Returns the string rep. of the base class"""
         return f"[{type(self).__name__}] ({self.id}) {self.__dict__}"
