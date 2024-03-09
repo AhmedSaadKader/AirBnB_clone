e072c6915a663c7d1133d4f3d70422b61d54e660
AhmedSaad <ahmed.saad.kader@gmail.com>
2024-03-09 23:55:06 +0200

    user

--- models/user.py
+++ models/user.py
@@ -10,10 +10,15 @@ class User(BaseModel):
     Args:
         BaseModel (BaseModel): super of class User
     """
-    def __init__(self, email="", password="",
-                 first_name="", last_name="", **kwargs):
-        self.email = email
-        self.password = password
-        self.first_name = first_name
-        self.last_name = last_name
-        super().__init__(**kwargs)
+    # def __init__(self, email="", password="",
+    #              first_name="", last_name="", **kwargs):
+    #     self.email = email
+    #     self.password = password
+    #     self.first_name = first_name
+    #     self.last_name = last_name
+    #     super().__init__(**kwargs)
+
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
