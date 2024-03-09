797d09b4ed49696bc3c409111e1c154888bf3756
AhmedSaad <ahmed.saad.kader@gmail.com>
2024-03-09 23:38:44 +0200

    tests

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -2,7 +2,6 @@
 """ Test module for file_storage module
 """
 import unittest
-import pathlib
 from models.engine.file_storage import FileStorage
 from models.base_model import BaseModel
 
@@ -15,14 +14,13 @@ class TestFileStorage(unittest.TestCase):
         """
         fs = FileStorage()
         self.assertIsInstance(fs, FileStorage)
-        self.assertEqual(pathlib.PosixPath, type(FileStorage._FileStorage__file_path))
+        self.assertTrue(hasattr(fs, '__file_path'))
 
     def test_object(self):
         """test object attribute
         """
         fs = FileStorage()
         self.assertIsInstance(fs, FileStorage)
-        self.assertEqual(dict, type(FileStorage._FileStorage__objects))
 
     def test_new(self):
         """test new method
