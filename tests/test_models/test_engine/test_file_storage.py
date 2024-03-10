60124215de1dc61b70940d5ad3e048ee86c39650
AhmedSaad <ahmed.saad.kader@gmail.com>
2024-03-10 11:34:27 +0200

    storage test

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -3,6 +3,7 @@
 """
 import unittest
 import pathlib
+from models import storage
 from models.engine.file_storage import FileStorage
 from models.base_model import BaseModel
 
@@ -13,39 +14,32 @@ class TestFileStorage(unittest.TestCase):
     def test_file_path(self):
         """test file path
         """
-        fs = FileStorage()
-        self.assertIsInstance(fs, FileStorage)
+
         self.assertEqual(pathlib.PosixPath, type(FileStorage._FileStorage__file_path))
 
     def test_object(self):
         """test object attribute
         """
-        fs = FileStorage()
-        self.assertIsInstance(fs, FileStorage)
         self.assertEqual(dict, type(FileStorage._FileStorage__objects))
 
     def test_new(self):
         """test new method
         """
-        fs = FileStorage()
         bm1 = BaseModel()
-        all_objects = fs.all()
+        all_objects = storage.all()
         self.assertIn(bm1, all_objects.values())
 
     def test_all(self):
         """test all method
         """
-        fs = FileStorage()
-        self.assertIsInstance(fs, FileStorage)
+        self.assertEqual(dict, type(storage.all()))
 
     def test_save(self):
         """test save method
         """
-        fs = FileStorage()
-        self.assertIsInstance(fs, FileStorage)
+        self.assertIsInstance(storage, FileStorage)
 
     def reload(self):
         """test reload method
         """
-        fs = FileStorage()
-        self.assertIsInstance(fs, FileStorage)
+        self.assertIsInstance(storage, FileStorage)
