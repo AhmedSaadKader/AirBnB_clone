d7ead7e5e16b69168f16a3430fc70fe133ecc316
moharafat <144336471+moharafat@users.noreply.github.com>
2024-03-10 19:48:27 +0200

    s

--- console.py
+++ console.py
@@ -5,9 +5,16 @@ class HBNBCommand(cmd.Cmd):
     prompt = "(hbnb) "
     def do_EOF(self, arg):
         return True
-    def do_Quit(self, arg):
+    def do_quit(self, arg):
         return True
     def emptyline(self):
         pass
+    def do_create(self, arg):
+        if len(arg) == 0:
+            print("** class name missing **")
+        elif arg[0] not in HBNBCommand.__classes:
+            print("** class doesn't exist **")
+
+    def do_show(self, arg):
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
